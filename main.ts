3e8cde5d9b70bb0d7757504a34e6c55f98847c6a
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-10-23 11:08:05 -0400

    added servo code

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2024 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Peter Zerbinos
+ * Created on: Sep 2024
+ * This program communicates to servo motor
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+//turn servo motor 0 degrees
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(servoNumber1, 0)
+    basic.clearScreen()
+    basic.showString('0')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+//turn servo motor 180 degrees
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber1, 180)
+    basic.clearScreen()
+    basic.showString('180')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
